52bee9970c96927997dfa678c9333318d9f91d59
Nick Baynham <nick.baynham@oracle.com>
2020-12-30 14:05:14 -0500

    added application Sprint Boot code to the project

---
+++ cicdpipeline/src/test/java/com/github/nickbaynham/cicdpipeline/CicdpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nickbaynham.cicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
